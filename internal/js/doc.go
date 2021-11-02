9f078a18e3292953a15c4827db964f68c4e867e8
rj <>
2021-11-02 15:05:57 +0000

    Update internal package documentation
    This commit updates the top-level package documentation for internal/js
    and internal/windows.

---
+++ internal/js/doc.go
@@ -0,0 +1,9 @@
+// Package js provides platform-dependent routines required to support the
+// package goey. In particular, on JS/WASM, most of the platform specific
+// support comes from the package syscall/js.  This package provides helpers for
+// creating and updating callbacks from JavaScript events back to Go.
+//
+// This package is intended for internal use.
+//
+// This package contains platform-specific details.
+package js
--- internal/windows/doc.go
+++ internal/windows/doc.go
@@ -1,10 +1,17 @@
 // Package windows provides platform-dependent routines required to support the
-// package goey.
-// In particular, on WIN32, the goal is to fill in some missing APIs that are not
-// provided by lxn's WIN32 binding.
-// Anything found herein should be a candidate for upstreaming.
-// Since the WIN32 naming convention  is also camel case, most of the functions
-// in this package are named exactly as their C API counterpart.
+// package goey when targeting WIN32.
+//
+// In particular, the goal of this package is to fill in some missing APIs that
+// are not provided by lxn's WIN32 binding.  Most functions found herein should
+// be a candidate for upstreaming. Since the WIN32 naming convention is also
+// camel case, most of the functions in this package are named exactly as their
+// C API counterpart.
+//
+// Some functions provided by lxn's WIN32 binding are recreated in this package.
+// Unfortunately, the functions provided by lxn eat error codes, which
+// complicates proper error handling.  Certain critical functions have been
+// rewritten to support error handling. See golang.org/x/sys/windows/mkwinsyscall
+// for guidance on proper wrapping of WIN32 API calls.
 //
 // This package is intended for internal use.
 //
